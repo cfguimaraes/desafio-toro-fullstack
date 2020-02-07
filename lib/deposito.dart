c48bc1d1976ed43c33ad1835fbd9d27b7541c6eb
Cleverton Fernandes Guimarães <cfguimaraes@debian.cleverton.me>
2020-02-07 19:58:44 -0300

    Telas para deposito e saque

--- lib/deposito.dart
+++ lib/deposito.dart
@@ -5,6 +5,38 @@ class Deposito extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Depositar"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              hintText: "R\$ 1234,00",
+              labelText: "Valor",
+            ),
+          ),
+          ButtonBar(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancelar e voltar"),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text("Limpar"),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text("Depositar"),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/saque.dart
+++ lib/saque.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
 class Saque extends StatelessWidget {
-static String route = "/saque";
+  static String route = "/saque";
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sacar"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              hintText: "R\$ 1234,00",
+              labelText: "Valor",
+            ),
+          ),
+          ButtonBar(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancelar e voltar"),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text("Limpar"),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text("Sacar"),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
